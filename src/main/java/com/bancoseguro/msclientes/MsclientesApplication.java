9c087a6bcc7414f807d821c36bbfa9155d1c60fc
Luis Delgado <37703993+delgadol@users.noreply.github.com>
2023-07-07 12:37:28 -0500

    Inicializacion
    Inicializacion

---
+++ src/main/java/com/bancoseguro/msclientes/MsclientesApplication.java
@@ -0,0 +1,13 @@
+package com.bancoseguro.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientesApplication.class, args);
+	}
+
+}
